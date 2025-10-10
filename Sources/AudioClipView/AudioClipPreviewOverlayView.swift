f20666af43928548f6abc4a6806bc9cb45561cc6
Lakr <git@qaq.wiki>
2025-10-11 00:21:41 +0800

    Update AudioClipPreviewOverlayView.swift

--- Sources/AudioClipView/AudioClipPreviewOverlayView.swift
+++ Sources/AudioClipView/AudioClipPreviewOverlayView.swift
@@ -31,9 +31,14 @@ public final class AudioClipPreviewOverlayView: UIView {
         _commonInit()
     }
 
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        _commonInit()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        _commonInit()
     }
 
     private func _commonInit() {
